ff4c362677f84b13c15d697e40de74ccb122d289
Данил <danikkach666@gmail.com>
2023-12-04 18:17:55 +0200

    add

---
+++ js/task-01.js
@@ -0,0 +1,15 @@
+const listWithId = document.querySelector('#categories');
+listWithId;
+console.log(listWithId);
+
+const listWithTotal = document.querySelector('li.item');
+listWithTotal;
+console.log(listWithTotal);
+
+const listWithUper = document.querySelectorAll('li.h2');
+listWithUper;
+console.log(listWithUper);
+
+const listWithClass = document.querySelectorAll('li');
+listWithClass;
+console.log(listWithClass);
